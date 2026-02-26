39c0dfffc7af0e9f8a6453b7b0e69bb99baf1d26
noble990921 <noble990921@gmail.com>
2026-02-26 16:16:32 +0900

    etc

--- src/components/category/detailBottomTab/NobleDetailBottom.vue
+++ src/components/category/detailBottomTab/NobleDetailBottom.vue
@@ -37,69 +37,73 @@
           <img :src="typeof img === 'string' ? img : img.url">
         </div>
       </div>
-      <div class="detail_sizeImg" ref="detail_sizeImg" v-if="product.sizeData && product.sizeData.img">
-        <!-- 가방 템플릿 -->
-        <template v-if="product.sizeData.type === 'bag'">
-          <img :src="product.sizeData.img">
-          <table class="size_table">
-            <thead>
-            <tr><th>cm</th><th>높이</th><th>너비</th><th>폭</th></tr>
-            </thead>
-            <tbody v-for="(p,idx) in product.sizeData.size" :key="idx">
-            <tr><th>{{p.unit || '-'}}</th><td>{{p.z || '-'}}</td><td>{{p.x || '-'}}</td><td>{{p.y || '-'}}</td></tr>
-            </tbody>
-          </table>
-          <p class="notice">*사이즈는 측정 기준에 따라 오차가 있을 수 있습니다.*</p>
-        </template>
+      <div class="detail_sizeImg" ref="detail_sizeImg" v-if="product.sizeData && product.sizeData.img"
+           :class="{ 'blur-overlay': !isLogin || !user }">
+        <p class="title" style="margin: 120px 0 30px 0">사이즈</p>
+        <div>
+          <!-- 가방 템플릿 -->
+          <template v-if="product.sizeData.type === 'bag'">
+            <img :src="product.sizeData.img">
+            <table class="size_table">
+              <thead>
+              <tr><th>cm</th><th>높이</th><th>너비</th><th>폭</th></tr>
+              </thead>
+              <tbody v-for="(p,idx) in product.sizeData.size" :key="idx">
+              <tr><th>{{p.unit || '-'}}</th><td>{{p.z || '-'}}</td><td>{{p.x || '-'}}</td><td>{{p.y || '-'}}</td></tr>
+              </tbody>
+            </table>
+            <p class="notice">*사이즈는 측정 기준에 따라 오차가 있을 수 있습니다.*</p>
+          </template>
 
-        <!-- 아우터/상의 템플릿 -->
-        <template v-else-if="isOuterOrTop">
-          <img :src="product.sizeData.img">
-          <table class="size_table" v-if="product.sizeData.size && product.sizeData.size.length > 0">
-            <thead>
-            <tr><th>cm</th><th>총장</th><th>어깨너비</th><th>가슴단면</th><th>소매길이</th></tr>
-            </thead>
-            <tbody v-for="(p,idx) in product.sizeData.size" :key="idx">
-            <tr><th>{{p.unit || '-'}}</th><td>{{p.totalLength || '-'}}</td><td>{{p.shoulderWidth || '-'}}</td><td>{{p.chestSection || '-'}}</td><td>{{p.sleeveLength || '-'}}</td></tr>
-            </tbody>
-          </table>
-          <p class="notice">*사이즈는 상품별로 약간의 편차가 있을 수 있습니다.*</p>
-        </template>
+          <!-- 아우터/상의 템플릿 -->
+          <template v-else-if="isOuterOrTop">
+            <img :src="product.sizeData.img">
+            <table class="size_table" v-if="product.sizeData.size && product.sizeData.size.length > 0">
+              <thead>
+              <tr><th>cm</th><th>총장</th><th>어깨너비</th><th>가슴단면</th><th>소매길이</th></tr>
+              </thead>
+              <tbody v-for="(p,idx) in product.sizeData.size" :key="idx">
+              <tr><th>{{p.unit || '-'}}</th><td>{{p.totalLength || '-'}}</td><td>{{p.shoulderWidth || '-'}}</td><td>{{p.chestSection || '-'}}</td><td>{{p.sleeveLength || '-'}}</td></tr>
+              </tbody>
+            </table>
+            <p class="notice">*사이즈는 상품별로 약간의 편차가 있을 수 있습니다.*</p>
+          </template>
 
-        <!-- 하의 템플릿 -->
-        <template v-else-if="isBottom">
-          <img :src="product.sizeData.img">
-          <table class="size_table" v-if="product.sizeData.size && product.sizeData.size.length > 0">
-            <thead>
-            <tr><th></th><th>총장</th><th>허리단면</th><th>엉덩이단면</th><th>허벅지단면</th><th>밑위</th><th>밑위단면</th></tr>
-            </thead>
-            <tbody v-for="(p,idx) in product.sizeData.size" :key="idx">
-            <tr><th>{{p.unit || '-'}}</th><td>{{p.totalLength || '-'}}</td><td>{{p.waistSection || '-'}}</td><td>{{p.hipSection || '-'}}</td>
-              <td>{{p.thighSection || '-'}}</td><td>{{p.rise || '-'}}</td><td>{{p.hemSection || '-'}}</td></tr>
-            </tbody>
-          </table>
-          <p class="notice">*참고: 위 치수는 유사 제품의 평균적인 수치를 기반으로 한 추정치이며, 실제 제품과 차이가 있을 수 있습니다.*</p>
-        </template>
+          <!-- 하의 템플릿 -->
+          <template v-else-if="isBottom">
+            <img :src="product.sizeData.img">
+            <table class="size_table" v-if="product.sizeData.size && product.sizeData.size.length > 0">
+              <thead>
+              <tr><th></th><th>총장</th><th>허리단면</th><th>엉덩이단면</th><th>허벅지단면</th><th>밑위</th><th>밑위단면</th></tr>
+              </thead>
+              <tbody v-for="(p,idx) in product.sizeData.size" :key="idx">
+              <tr><th>{{p.unit || '-'}}</th><td>{{p.totalLength || '-'}}</td><td>{{p.waistSection || '-'}}</td><td>{{p.hipSection || '-'}}</td>
+                <td>{{p.thighSection || '-'}}</td><td>{{p.rise || '-'}}</td><td>{{p.hemSection || '-'}}</td></tr>
+              </tbody>
+            </table>
+            <p class="notice">*참고: 위 치수는 유사 제품의 평균적인 수치를 기반으로 한 추정치이며, 실제 제품과 차이가 있을 수 있습니다.*</p>
+          </template>
 
-        <!-- 원피스/스커트 템플릿 -->
-        <template v-else-if="product.sizeData.type === 'dressskirt'">
-          <img :src="product.sizeData.img">
-          <table class="size_table" v-if="product.sizeData.size && product.sizeData.size.length > 0">
-            <thead>
-            <tr><th></th><th>총기장</th><th>허리단면</th><th>밑단단면</th></tr>
-            </thead>
-            <tbody v-for="(p,idx) in product.sizeData.size" :key="idx">
-            <tr><th>{{p.unit || '-'}}</th><td>{{p.totalLength || '-'}}</td><td>{{p.waistSection || '-'}}</td><td>{{p.hemSection || '-'}}</td></tr>
-            </tbody>
-          </table>
-          <p class="notice">*사이즈는 상품별로 약간의 편차가 있을 수 있습니다.*</p>
-        </template>
+          <!-- 원피스/스커트 템플릿 -->
+          <template v-else-if="product.sizeData.type === 'dressskirt'">
+            <img :src="product.sizeData.img">
+            <table class="size_table" v-if="product.sizeData.size && product.sizeData.size.length > 0">
+              <thead>
+              <tr><th></th><th>총기장</th><th>허리단면</th><th>밑단단면</th></tr>
+              </thead>
+              <tbody v-for="(p,idx) in product.sizeData.size" :key="idx">
+              <tr><th>{{p.unit || '-'}}</th><td>{{p.totalLength || '-'}}</td><td>{{p.waistSection || '-'}}</td><td>{{p.hemSection || '-'}}</td></tr>
+              </tbody>
+            </table>
+            <p class="notice">*사이즈는 상품별로 약간의 편차가 있을 수 있습니다.*</p>
+          </template>
 
-        <!-- 신발 템플릿 (사진만) -->
-        <template v-else-if="product.sizeData.type === 'shoes'">
-          <img :src="product.sizeData.img">
-          <p class="notice">*발볼 넓이 및 발등 높이에 따라 착화감이 다를 수 있습니다.*</p>
-        </template>
+          <!-- 신발 템플릿 (사진만) -->
+          <template v-else-if="product.sizeData.type === 'shoes'">
+            <img :src="product.sizeData.img">
+            <p class="notice">*발볼 넓이 및 발등 높이에 따라 착화감이 다를 수 있습니다.*</p>
+          </template>
+        </div>
       </div>
       <div class="detail_notice" ref="detail_notice"
            :class="{ 'blur-overlay': !isLogin || !user }">
